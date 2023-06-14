f44afa812186618f67bd195d7d2fdd74a5b363f8
NOISE <46085246+rcy1314@users.noreply.github.com>
2023-06-15 04:43:44 +0800

    Update Bingtrans.py

--- Bingtrans.py
+++ Bingtrans.py
@@ -36,7 +36,7 @@ class BingTran:
     def get_newcontent(self, max_item=2):
         item_list = []
         if len(self.d.entries) < max_item:
-            max_item = len(self.d。entries)
+            max_item = len(self.d.entries)
         for entry 在 self.d。entries[:max_item]:
             try:
                 title = self.tr(entry.title)
