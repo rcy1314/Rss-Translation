d942f95eb516b9cc388a69e97b4caa559be2e132
NOISE <1129159201@qq.com>
2023-06-15 18:11:43 +0800

    重构配置

--- Bingtrans.py
+++ Bingtrans.py
@@ -115,14 +115,14 @@ def update_readme():
 def tran(sec):
     # 获取各种配置信息
     out_dir = os.path.join(BASE, get_cfg(sec, 'name'))
-    xml_file = os.path.join(BASE, f'{get_cfg(sec, "name")}.xml')
+    ini_name = get_cfg(sec, 'name')
     url = get_cfg(sec, 'url')
     max_item = int(get_cfg(sec, 'max'))
     old_md5 = get_cfg(sec, 'md5') 
     # 读取旧的 MD5 散列值
     source, target = get_cfg_tra(sec)
     global links
-    links += [" - %s [%s](%s) -> [%s](%s)\n" % (sec, url, (url), get_cfg(sec, 'name'), parse.quote(xml_file))]
+    links += [" - %s [%s](%s) -> [%s](%s)\n" % (sec, url, (url), ini_name, parse.quote(out_dir))]
 
     # 获取新的 RSS 内容，并计算新的 MD5 散列值
     new_content = BingTran(url, target=target, source=source).get_newcontent(max_item=max_item)
@@ -159,23 +159,24 @@ def tran(sec):
     rss_description = feed["description"]
     rss_last_build_date = feed["lastBuildDate"].strftime('%a, %d %b %Y %H:%M:%S GMT')
 
-    template = Template("""<rss version="2.0">
-        <channel>
-            <title>{{ rss_title }}</title>
-            <link>{{ rss_link }}</link>
-            <description>{{ rss_description }}</description>
-            <lastBuildDate>{{ rss_last_build_date }}</lastBuildDate>
-            {% for item in rss_items -%}
-            <item>
-                <title>{{ item.title }}</title>
-                <link>{{ item.link }}</link>
-                <description>{{ item.description }}</description>
-                <guid isPermaLink="false">{{ item.guid }}</guid>
-                <pubDate>{{ item.pubDate.strftime('%a, %d %b %Y %H:%M:%S GMT') }}</pubDate>
-            </item>
-            {% endfor -%}
-        </channel>
-    </rss>""")
+    template = Template("""<?xml version="1.0" encoding="UTF-8"?>
+        <rss version="2.0">
+            <channel>
+                <title>{{ rss_title }}</title>
+                <link>{{ rss_link }}</link>
+                <description>{{ rss_description }}</description>
+                <lastBuildDate>{{ rss_last_build_date }}</lastBuildDate>
+                {% for item in rss_items -%}
+                <item>
+                    <title>{{ item.title }}</title>
+                    <link>{{ item.link }}</link>
+                    <description>{{ item.description }}</description>
+                    <guid>{{ item.guid }}</guid>
+                    <pubDate>{{ item.pubDate.strftime('%a, %d %b %Y %H:%M:%S GMT') }}</pubDate>
+                </item>
+                {% endfor -%}
+            </channel>
+        </rss>""")
 
     rss = template.render(rss_title=rss_title, rss_link=rss_link, rss_description=rss_description, rss_last_build_date=rss_last_build_date, rss_items=rss_items)
 
@@ -185,27 +186,34 @@ def tran(sec):
         print("Error occurred when creating directory %s: %s" % (BASE, str(e)))
         return
 
-    # 如果 RSS 文件存在，则将新内容追加到原有内容后面
-    if os.path.isfile(xml_file):
-        try:
-            with open(xml_file, 'r', encoding='utf-8') as f:
-                old_rss = f.read()
-        except Exception as e:
-            print("Error occurred when reading RSS file %s for %s: %s" % (xml_file, sec, str(e)))
-            return
-        rss = old_rss + rss
-
     try:
-        with open(xml_file, 'w', encoding='utf-8') as f:
+        with open(out_dir, 'w', encoding='utf-8') as f:
             f.write(rss)
     except Exception as e:
-        print("Error occurred when writing RSS file %s for %s: %s" % (xml_file, sec, str(e)))
+        print("Error occurred when writing RSS file %s for %s: %s" % (out_dir, sec, str(e)))
         return
 
     # 更新配置信息并写入文件中
     set_cfg(sec, 'md5', new_md5)
     with open('test.ini', "w") as configfile:
         config.write(configfile)
+
+    # 将新内容追加到原有内容后面
+    try:
+        feed = feedparser.parse(out_dir)
+    except FileNotFoundError:
+        print("RSS文件不存在，将创建新文件")
+        feed = feedparser.FeedParserDict()
+        feed.entries = []
+    entry = feedparser.FeedParserDict()
+    entry.title = rss_title
+    entry.link = rss_link
+    entry.description = rss_description
+    feed.entries.append(entry)
+    with open(out_dir, 'w') as f:
+        f.write(feed.to_xml())
+
+    print("新内容已成功追加到RSS文件中")
 
 # 遍历所有的 RSS 配置，依次更新 RSS 文件
 config = configparser.ConfigParser()
